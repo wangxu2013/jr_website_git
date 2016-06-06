cc016f4a5e0e819c97d4267131f42eef5f1404c1
WangXu <qk_wangxu@163.com>
2016-06-06 09:35:14 +0800

    [mod]链接、响应式布局测试

--- scapp/views/index.py
+++ scapp/views/index.py
@@ -10,109 +10,60 @@ def index():
     return render_template("index.html")
 
 # 首页
-@app.route('/index_En')
-def index_En():
-    return render_template("index_En.html")
+@app.route('/index_EN')
+def index_EN():
+    return render_template("index_EN.html")
 
-# 关于我们
-@app.route('/gsjj')
-def gsjj():
-    return render_template("gywm/gsjj.html")
+# 乾康领域
+@app.route('/qkly')
+def qkly():
+    return render_template("qkly/qkly.html")
 
-# 关于我们
-@app.route('/gsjj_En')
-def gsjj_En():
-    return render_template("gywm/gsjj_En.html")
+# 乾康领域
+@app.route('/qkly_EN')
+def qkly_EN():
+    return render_template("qkly/qkly_EN.html")
 
-# 新闻动态
-@app.route('/xwdt')
-def xwdt():
-    return render_template("xwdt/xwdt.html")
+# 乾康伙伴
+@app.route('/qkhb')
+def qkhb():
+    return render_template("qkhb/qkhb.html")
 
-# 新闻动态
-@app.route('/xwdt_En')
-def xwdt_EN():
-    return render_template("xwdt/xwdt.html")
+# 乾康伙伴
+@app.route('/qkhb_EN')
+def qkhb_EN():
+    return render_template("qkhb/qkhb_EN.html")
 
-# 解决方案
-@app.route('/jjfa')
-def jjfa():
-    return render_template("jjfa/jjfa.html")
+# 乾康家族
+@app.route('/qkjz')
+def qkjz():
+    return render_template("qkjz/qkjz.html")
 
-# 解决方案
-@app.route('/jjfa_En')
-def jjfa_En():
-    return render_template("jjfa/jjfa_En.html")
+# 乾康家族
+@app.route('/qkjz_EN')
+def qkjz_EN():
+    return render_template("qkjz/qkjz_EN.html")
 
-# 银行内训
-@app.route('/yhnx')
-def yhnx():
-    return render_template("yhnx/yhnx.html")
+# 加入乾康
+@app.route('/jrqk')
+def jrqk():
+    return render_template("jrqk/jrqk.html")
 
-# 银行内训
-@app.route('/yhnx_En')
-def yhnx_En():
-    return render_template("yhnx/yhnx_En.html")
+# 加入乾康
+@app.route('/jrqk_EN')
+def jrqk_EN():
+    return render_template("jrqk/jrqk_EN.html")
 
-# 成功案例
-@app.route('/cgal')
-def cgal():
-    return render_template("cgal/cgal.html")
+# 关于乾康
+@app.route('/gyqk')
+def gyqk():
+    return render_template("gyqk/gyqk.html")
 
-# 成功案例
-@app.route('/cgal_En')
-def cgal_En():
-    return render_template("cgal/cgal_En.html")
+# 关于乾康
+@app.route('/gyqk_EN')
+def gyqk_EN():
+    return render_template("gyqk/gyqk_EN.html")
 
-# 合作伙伴
-@app.route('/hzhb')
-def hzhb():
-    return render_template("hzhb/hzhb.html")
-
-# 合作伙伴
-@app.route('/hzhb_En')
-def hzhb_En():
-    return render_template("hzhb/hzhb_En.html")
-
-# 金融咨询
-@app.route('/jrzx')
-def jrzx():
-    return render_template("jrzx/jrzx.html")
-
-# 金融咨询
-@app.route('/jrzx_En')
-def jrzx_En():
-    return render_template("jrzx/jrzx_En.html")
-
-# 需求反馈
-@app.route('/xqfk')
-def xqfk():
-    return render_template("xqfk/xqfk.html")
-
-# 需求反馈
-@app.route('/xqfk_En')
-def xqfk_En():
-    return render_template("xqfk/xqfk_En.html")
-	
-# 员工风采
-@app.route('/ygfc')
-def ygfc():
-    return render_template("ygfc/ygfc.html")
-
-# 员工风采
-@app.route('/ygfc_En')
-def ygfc_En():
-    return render_template("ygfc/ygfc_En.html")
-
-# 招聘信息
-@app.route('/zpxx')
-def zpxx():
-    return render_template("zpxx/zpxx.html")
-
-# 招聘信息
-@app.route('/zpxx_En')
-def zpxx_En():
-    return render_template("zpxx/zpxx_En.html")
 
 # 管理员
 @app.route('/admin')
